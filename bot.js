1e87d222c3932863a600a48c644a06d6ca061ead
motichaimo-droid <motichaimo@gmail.com>
2026-04-09 19:14:41 +0300

    use system chromium on render

--- bot.js
+++ bot.js
@@ -5,11 +5,12 @@ const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    executablePath: '/usr/bin/chromium',
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
 });
 
 // טען טקסט
